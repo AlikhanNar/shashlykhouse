538da6a4bcb0bde27cd63dcfc35ccfd32aa88376
Alikhan <anarykbaev@gmail.com>
2024-09-13 14:07:44 +0500

    test
    test

---
+++ js/lang.js
@@ -0,0 +1,27 @@
+const langArr = {
+    "unit" :  {
+        "ru": "ШашлыкHouse",
+        "en": "ShashlykHouse ",
+        "kz": "ШашлыкHouse",
+    }, 
+    "rest1": {
+        "ru": "Ресторан",
+        "en": "Restaurant ",
+        "kz": "Мейрамхана ",
+    }, 
+    "rest2": {
+        "ru": "• Сочный шашлык •",
+        "en": "• Juicy kebab •",
+        "kz": "• Шырынды шашлык •",
+    }, 
+    "rest3": {
+        "ru": "• Altyn Joja •",
+        "en": "• Altyn Joja •",
+        "kz": "• Altyn Joja •",
+    }, 
+    "rest4": {
+        "ru": "• VIP комнаты, Тапчаны •",
+        "en": "• VIP rooms, Tapchany •",
+        "kz": "• VIP бөлмелер, Тапчандар •",
+    },
+}
